157b78646e4549f78579f159d7699c1dcc3da53d
sudhansusekhat <sudhansusekharmahapatra@outlook.com>
2016-05-10 16:16:15 +0530

    utest

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -12,12 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView textView;
+
+    Button b;
 
 
     @Override
